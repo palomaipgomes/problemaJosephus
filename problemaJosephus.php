0a6affb3c7820e003c110cd3df6f41b987f92f3f
Paloma Gomes <contato.palomaipgomes@gmail.com>
2021-05-17 21:01:45 -0300

    Create problemaJosephus.php
    Resolvendo o problema de Josephus

---
+++ problemaJosephus.php
@@ -0,0 +1,41 @@
+<?php
+
+function problemaJosephus($nPessoas, $posInicio) {
+	$arr = array();
+	for($i = 0; $i < $nPessoas; $i++) {
+		array_push($arr, ($i+1));
+	}
+
+	$calculoRemocaoPosicao = $posInicio - 1;
+	$iteracoes = $nPessoas - 1;
+	while($iteracoes > 0) {
+		unset($arr[$calculoRemocaoPosicao]);
+
+		$arr = array_values($arr);
+
+		$calculoRemocaoPosicao += $posInicio - 1;
+		if ($calculoRemocaoPosicao > (count($arr) - 1)) {
+			$calculoRemocaoPosicao = $calculoRemocaoPosicao % count($arr);
+		}
+		$iteracoes--;
+	}
+	return current($arr);
+}
+
+$vivo = problemaJosephus(5, 3);
+echo ('A posição é ' . $vivo);
+echo "\n";
+$vivo = problemaJosephus(10, 3);
+echo ('A posição é ' . $vivo);
+echo "\n";
+$vivo = problemaJosephus(5, 2);
+echo ('A posição é ' . $vivo);
+echo "\n";
+$vivo = problemaJosephus(7, 3);
+echo ('A posição é ' . $vivo);
+echo "\n";
+$vivo = problemaJosephus(13, 2);
+echo ('A posição é ' . $vivo);
+echo "\n";
+
+?>
